fb5ae0e467d9a9842d167e2325646f445b576c45
Birat Poudel <poudel.birat25@gmail.com>
2023-10-14 13:26:04 +0545

    07-spring-boot-rest-crud: performed basic read operations

---
+++ 07-spring-boot-rest-crud/src/main/java/com/biratpoudel/demo/rest/StudentRestController.java
@@ -0,0 +1,40 @@
+package com.biratpoudel.demo.rest;
+
+import com.biratpoudel.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+
+        students = new ArrayList<>();
+
+        students.add(new Student("Birat", "Poudel"));
+        students.add(new Student("Sijan", "Bhattarai"));
+        students.add(new Student("Dipu", "Dahal"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        return students.get(studentId);
+    }
+}
