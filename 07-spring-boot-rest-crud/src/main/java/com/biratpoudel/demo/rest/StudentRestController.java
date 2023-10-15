b002eed18a224f3d641f923e8441eb1f54ac7714
Birat Poudel <poudel.birat25@gmail.com>
2023-10-15 19:21:29 +0545

    07-spring-boot-rest-crud: Exceptions Handling done

--- 07-spring-boot-rest-crud/src/main/java/com/biratpoudel/demo/rest/StudentRestController.java
+++ 07-spring-boot-rest-crud/src/main/java/com/biratpoudel/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.biratpoudel.demo.rest;
 
 import com.biratpoudel.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +33,10 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+
+        if (studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student Id not found " + studentId);
+        }
 
         return students.get(studentId);
     }
