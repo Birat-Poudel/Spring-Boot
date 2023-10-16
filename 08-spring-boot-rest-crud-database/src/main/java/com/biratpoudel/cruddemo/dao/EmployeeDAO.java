e34a05b9729d7705ffa5284e07724e7f8ebc7625
Birat Poudel <poudel.birat25@gmail.com>
2023-10-16 17:54:11 +0545

    08-spring-boot-rest-crud-database: REST crud operations

---
+++ 08-spring-boot-rest-crud-database/src/main/java/com/biratpoudel/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,13 @@
+package com.biratpoudel.cruddemo.dao;
+
+import com.biratpoudel.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+    Employee findById(int id);
+    Employee save(Employee employee);
+    void deleteById(int id);
+}
---
+++ 08-spring-boot-rest-crud-database/src/main/java/com/biratpoudel/cruddemo/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.biratpoudel.cruddemo.service;
+
+import com.biratpoudel.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+    Employee findById(int id);
+    Employee save(Employee employee);
+    void deleteById(int id);
+}
